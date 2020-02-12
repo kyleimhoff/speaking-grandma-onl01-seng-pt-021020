42dd6ec69b2511613d76f033b1737c06f7797b6f
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-12 02:07:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ phrase = "hi grandma"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == (#{phrase}).upcase
     puts "NO, NOT SINCE 1938!"
-  elsif
+  else
     puts "HUH, SPEAK UP SONNY!"
   end
   
